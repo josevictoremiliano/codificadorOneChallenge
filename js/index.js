86222e09252bde5a1037a28cedfb11ca4fda2ccb
josevictoremiliano <josevictoremiliano@gmail.com>
2024-08-20 11:14:39 -0300

    ✨ NEW:  Adicionado Botão de linguagem e tradução

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 const criptografarBtn = document.getElementById('btn-criptografar');
 const descriptografarBtn = document.getElementById('btn-descriptografar');
 const textArea = document.getElementById('frase');
+const aviso = document.getElementById('avisoDeLetrasTexto');
+
+const footer = document.getElementById('footer-text');
 
 const resultado = document.getElementsByClassName('textoResult');
 const copiar = document.getElementById('btn-copiar');
@@ -12,10 +15,23 @@ const resposta = document.getElementsByClassName('resposta')
 const alerta = document.getElementsByClassName('avisos');
 const colar = document.getElementById('btn-colar');
 
+const buttons = document.querySelectorAll('.btn-linguagem');
+const pt = document.getElementById('btn-pt');
+const es = document.getElementById('btn-es');
+const en = document.getElementById('btn-en');
+
+const preRespostasH5 = document.getElementById('TextoSemMensagem');
+const preRespostasSpan = document.getElementById('SpanDoTextoSemMensagem')
+
+
 // criptografar
 criptografarBtn.addEventListener('click', () => {
     if (textArea.value === '') {
-        alerta[0].innerHTML = '<i class="mif-cross-light"></i> Digite algo para ser criptografado';
+        if (pt.classList.contains('active')) {
+            alerta[0].innerHTML = '<i class="mif-cross-light"></i> Digite algo para ser criptografado';
+        } else {
+            alerta[0].innerHTML = '<i class="mif-cross-light"></i> Escribe algo para cifrar';
+        }
         alerta[0].style.display = 'block';
         alerta[0].classList.add('error');
         setTimeout(() => {
@@ -35,7 +51,7 @@ criptografarBtn.addEventListener('click', () => {
 
         preResposta[0].style.display = 'none';
         resposta[0].style.display = 'flex';
-        
+
         textArea.value = '';
     }
 });
@@ -43,7 +59,11 @@ criptografarBtn.addEventListener('click', () => {
 // descriptografar
 descriptografarBtn.addEventListener('click', () => {
     if (textArea.value === '') {
-        alerta[0].innerHTML = '<i class="mif-cross-light"></i> Digite algo para ser descriptografado';
+        if (pt.classList.contains('active')) {
+            alerta[0].innerHTML = '<i class="mif-cross-light"></i> Digite algo para ser descriptografado';
+        } else {
+            alerta[0].innerHTML = '<i class="mif-cross-light"></i> Escribe algo para descifrar';
+        }
         alerta[0].style.display = 'block';
         alerta[0].classList.add('error');
         setTimeout(() => {
@@ -55,7 +75,7 @@ descriptografarBtn.addEventListener('click', () => {
         const frase = textArea.value;
         console.log(frase);
         const descriptografada = frase.replace(/1f/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
-        
+
 
         resultado[0].innerHTML = descriptografada;
 
@@ -71,21 +91,72 @@ copiar.addEventListener('click', () => {
     const copiado = resultado[0].innerText;
     navigator.clipboard.writeText(copiado);
     copiar.innerHTML = '<i class="mif-clipboard"></i> Copiado!  ';
+
 });
 
 // colar
 colar.addEventListener('click', () => {
-   if (navigator.clipboard.readText) {
-       navigator.clipboard.readText().then(text => {
-           textArea.value = text;
-       });
-   } else {
-       alerta[0].innerHTML = '<i class="mif-cross-light"></i> Não foi possível colar';
-       alerta[0].style.display = 'block';
-       alerta[0].classList.add('error');
-       setTimeout(() => {
-           alerta[0].style.display = 'none';
-       }, 3000);
-   }
+    if (navigator.clipboard.readText) {
+        navigator.clipboard.readText().then(text => {
+            textArea.value = text;
+        });
+    } else {
+        if (pt.classList.contains('active')) {
+            alerta[0].innerHTML = '<i class="mif-cross-light"></i> Não foi possível colar';
+        } else {
+            alerta[0].innerHTML = '<i class="mif-cross-light"></i> No se pudo pegar';
+        }
+        alerta[0].style.display = 'block';
+        alerta[0].classList.add('error');
+        setTimeout(() => {
+            alerta[0].style.display = 'none';
+        }, 3000);
+    }
 
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            buttons.forEach(btn => btn.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+
+    // se o botão pt for clicado troca o idioma para português e se o botão es for clicado troca o idioma para espanhol
+    pt.addEventListener('click', () => {
+        criptografarBtn.innerHTML = 'Criptografar';
+        descriptografarBtn.innerHTML = 'Descriptografar';
+        copiar.innerHTML = 'Copiar';
+        textArea.placeholder = 'Digite seu texto';
+        aviso.innerHTML = 'Apenas letras minúsculas e sem acento.';
+
+        preRespostasH5.innerHTML = 'Nenhuma mensagem encontrada';
+        preRespostasSpan.innerHTML = 'Digite um texto que você deseja criptografar ou descriptografar.';
+        footer.innerHTML = 'Feito com <span>❤</span> por <a href="https://github.com/josevictoremiliano">José Victor Emiliano da Silva</a> - 2024';
+    });
+
+    es.addEventListener('click', () => {
+        criptografarBtn.innerHTML = 'Cifrar';
+        descriptografarBtn.innerHTML = 'Descifrar';
+        copiar.innerHTML = 'Copiar';
+        textArea.placeholder = 'Escribe tu texto';
+        aviso.innerHTML = 'Solo letras minúsculas y sin acento.';
+        preRespostasH5.innerHTML = 'No se encontró ningún mensaje';
+        preRespostasSpan.innerHTML = 'Escribe un texto que quieras cifrar o descifrar.';
+        footer.innerHTML = 'Hecho con <span>❤</span> por <a href="https://github.com/josevictoremiliano">José Victor Emiliano da Silva</a> - 2024';
+    })
+
+    en.addEventListener('click', () => {
+        criptografarBtn.innerHTML = 'Encrypt';
+        descriptografarBtn.innerHTML = 'Decrypt';
+        copiar.innerHTML = 'Copy';
+        textArea.placeholder = 'Type your text';
+        aviso.innerHTML = 'Only lowercase letters and no accent.';
+        preRespostasH5.innerHTML = 'No message found';
+        preRespostasSpan.innerHTML = 'Type a text you want to encrypt or decrypt.';
+        footer.innerHTML = 'Made with <span>❤</span> by <a href="https://github.com/josevictoremiliano">José Victor Emiliano da Silva</a> - 2024'; 
+    })
+});
+
